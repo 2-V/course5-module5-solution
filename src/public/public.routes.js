ec3f44a53124e636ffc2ecc3e1c736d90ed367a1
Tim Filer <tim.j.filer@gmail.com>
2018-01-04 14:36:14 -0600

    Final Commit

--- src/public/public.routes.js
+++ src/public/public.routes.js
@@ -43,7 +43,20 @@ function routeConfig ($stateProvider) {
     })
     .state('public.signup', {
       url: '/signup',
-      templateUrl: 'src/public/signup/signup.html'
+      templateUrl: 'src/public/signup/signup.html',
+      controller: 'SignUpController',
+      controllerAs: 'signUpCtrl'
+    })
+    .state('public.myinfo', {
+      url: '/myinfo',
+      templateUrl: 'src/public/myinfo/myinfo.html',
+      controller: 'MyInfoController',
+      controllerAs: 'myInfoCtrl',
+      resolve: {
+        info: ['MyInfoService', function(MyInfoService) {
+          return MyInfoService.getInfo();
+        }]
+      }
     });
 }
 })();
